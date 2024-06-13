e4f70348606c10d7d329f5548f945601452335f7
li-guohao <git@liguohao.cn>
2024-06-13 13:44:22 +0800

    添加项目文件。

---
+++ MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWpfMusicPlayer
+{
+    internal class MainViewModel
+    {
+        public ObservableCollection<SongModel> Songs { get; set; }
+        public MainViewModel() {
+
+            string json = System.IO.File.ReadAllText("song.json");
+
+            List<SongModel>  songs = JsonConvert.DeserializeObject<List<SongModel>>(json);
+
+            Songs = new ObservableCollection<SongModel>(songs);
+        }
+
+    }
+}
