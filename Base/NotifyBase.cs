329fa1cbf75488fb5d055b6e09dd4fa7abbe4197
li-guohao <git@liguohao.cn>
2024-06-13 14:14:25 +0800

    播放模式图标操作

---
+++ Base/NotifyBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWpfMusicPlayer.Base
+{
+    internal class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void Notify([CallerMemberName] string propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using DemoWpfMusicPlayer.Base;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DemoWpfMusicPlayer
 {
-    internal class MainViewModel
+    internal class MainViewModel : NotifyBase
     {
         public ObservableCollection<SongModel> Songs { get; set; }
         public MainViewModel() {
@@ -20,5 +21,37 @@ namespace DemoWpfMusicPlayer
             Songs = new ObservableCollection<SongModel>(songs);
         }
 
+        int _playMode = 0;
+        private string _playModeStr = "\ue610";
+        public string PlayModeStr
+        {
+            get { return _playModeStr; }
+            set {
+                _playModeStr = value;
+                this.Notify();
+            }
+        }
+
+        public CommandBase PlayModeCommand { get => new CommandBase { DoExecute = DoPlayerModeCommand }; }
+
+        private void DoPlayerModeCommand(object sender)
+        {
+            _playMode = (_playMode + 1) % 3;
+            switch (_playMode)
+            {
+                case 0:
+                    PlayModeStr = "\ue610";
+                    break;
+                case 1:
+                    PlayModeStr = "\ue85a";
+                    break;
+                case 2:
+                    PlayModeStr = "\ue7b8";
+                    break;
+                default:
+                    PlayModeStr = "\ue610";
+                    break;
+            }
+        }
     }
 }
